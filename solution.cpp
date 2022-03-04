a197a6e6df6686cf280469f7fcfde74ad1c41ef9
maverick729 <42774971+maverick729@users.noreply.github.com>
2022-03-04 18:20:55 +0530

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -1,14 +1,132 @@
 //done by varunmaddipati
+#pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,fast-math,O3")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
 
 #include "bits/stdc++.h"
+#include "ext/pb_ds/assoc_container.hpp"
+#include "ext/pb_ds/tree_policy.hpp"
+using namespace __gnu_pbds;
 using namespace std;
 
+template<class T> 
+using ordered_set = tree<T, null_type,less<T>, rb_tree_tag, tree_order_statistics_node_update> ;
+ 
+template<class key, class value, class cmp = std::less<key>>
+using ordered_map = tree<key, value, cmp, rb_tree_tag, tree_order_statistics_node_update>;
+// find_by_order(k)  returns iterator to kth element starting from 0;
+// order_of_key(k) returns count of elements strictly smaller than k;
+ 
+template<class T>
+using min_heap = priority_queue<T,vector<T>,greater<T> >; 
 
-//------------------------------------Defines-------------------------------//
+/*/--------------------------------IO(Debugging)-----------------------------/*/
+template<class T> istream& operator >> (istream &is, vector<T>& V) {
+    for(auto &e : V)                                                          
+        is >> e;
+    return is;
+}
+template<class T, size_t N> istream& operator >> (istream &is, array<T, N>& V) {
+    for(auto &e : V)
+        is >> e;
+    return is;
+}
+#ifdef __SIZEOF_INT128__
+ostream& operator << (ostream &os, __int128 const& value){
+    static char buffer[64];
+    int index = 0;
+    __uint128_t T = (value < 0) ? (-(value + 1)) + __uint128_t(1) : value;
+    if (value < 0) 
+        os << '-';
+    else if (T == 0)
+        return os << '0';
+    for(; T > 0; ++index){
+        buffer[index] = static_cast<char>('0' + (T % 10));
+        T /= 10;
+    }    
+    while(index > 0)
+        os << buffer[--index];
+    return os;
+}
+istream& operator >> (istream& is, __int128& T){
+    static char buffer[64];
+    is >> buffer;
+    size_t len = strlen(buffer), index = 0;
+    T = 0; int mul = 1;
+    if (buffer[index] == '-')
+        ++index, mul *= -1;
+    for(; index < len; ++index)
+        T = T * 10 + static_cast<int>(buffer[index] - '0');
+    T *= mul;    
+    return is;
+}
+#endif
+template<typename CharT, typename Traits, typename T>
+ostream& _containerprint(std::basic_ostream<CharT, Traits> &out, T const &val) {
+    return (out << val << " ");
+}
+template<typename CharT, typename Traits, typename T1, typename T2>
+ostream& _containerprint(std::basic_ostream<CharT, Traits> &out, pair<T1, T2> const &val) {
+    return (out << "(" << val.first << "," << val.second << ") ");
+}
+template<typename CharT, typename Traits, template<typename, typename...> class TT, typename... Args>
+ostream& operator << (std::basic_ostream<CharT, Traits> &out, TT<Args...> const &cont) {
+    out << "[ ";
+    for(auto&& elem : cont) _containerprint(out, elem);
+    return (out << "]");
+}
+template<class L, class R> ostream& operator << (ostream& out, pair<L, R> const &val){
+    return (out << "(" << val.first << "," << val.second << ") ");
+}
+template<typename L, size_t N> ostream& operator << (ostream& out, array<L, N> const &cont){
+    out << "[ ";
+    for(auto&& elem : cont) _containerprint(out, elem);
+    return (out << "]");    
+}
+template<class T> ostream& operator<<(ostream &out, ordered_set<T> const& S){
+    out << "{ ";
+    for(const auto& s:S) out << s << " ";
+    return (out << "}");
+}
+template<class L, class R, class chash = std::hash<L> > ostream& operator << (ostream &out, gp_hash_table<L, R, chash> const& M) {
+    out << "{ ";
+    for(const auto& m: M) out << "(" << m.first << ":" << m.second << ") ";
+    return (out << "}");
+}
+template<class P, class Q = vector<P>, class R = less<P> > ostream& operator << (ostream& out, priority_queue<P, Q, R> const& M){
+    static priority_queue<P, Q, R> U;
+    U = M;
+    out << "{ ";
+    while(!U.empty())
+        out << U.top() << " ", U.pop();
+    return (out << "}");
+}
+template<class P> ostream& operator << (ostream& out, queue<P> const& M){
+    static queue<P> U;
+    U = M;
+    out << "{ ";
+    while(!U.empty())
+        out << U.front() << " ", U.pop();
+    return (out << "}");
+}
+#define TRACE
+#ifdef TRACE
+    #define trace(...) __f(#__VA_ARGS__, __VA_ARGS__)
+    template <typename Arg1>
+    void __f(const char* name, Arg1&& arg1){
+        cerr << name << " : " << arg1 << endl;
+    }
+    template <typename Arg1, typename... Args>
+    void __f(const char* names, Arg1&& arg1, Args&&... args){
+        const char* comma = strchr(names + 1, ',');
+        cerr.write(names, comma - names) << " : " << arg1<<" | ";
+        __f(comma+1, args...);
+    }
+#else
+    #define trace(...) 1
+#endif
+
 #define getline_clear cin.ignore()
 #define pb push_back
-
-//-----------------------------Code begins----------------------------------//
 
 string remove_quotes(string s);
 
